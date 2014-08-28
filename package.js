e04ed8c76c29c75813194362a55447b89bec1b0e
Ry Walker <ry@rywalker.com>
2014-08-27 23:25:02 -0500

    Finishing the job /hattip @rgoomar

--- package.js
+++ package.js
@@ -5,8 +5,9 @@
 ***************************************************************************/
 
 Package.describe({
-  name: 'file-collection',
-  summary: "Files stored in Meteor collections, based on MongoDB's GridFS filestore"
+  name: 'filecollection',
+  summary: "Files stored in Meteor collections, based on MongoDB's GridFS filestore",
+  git: "https://github.com/vsivsi/meteor-file-collection.git"
 });
 
 Npm.depends({
@@ -19,24 +20,24 @@ Npm.depends({
   'cookie-parser': '1.3.2'
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
 
   api.use('coffeescript', ['server','client']);
   api.use('webapp', 'server');
-  api.add_files('gridFS.coffee', ['server','client']);
-  api.add_files('resumable/resumable.js', 'client')
-  api.add_files('server_shared.coffee', 'server');
-  api.add_files('gridFS_server.coffee', 'server');
-  api.add_files('resumable_server.coffee', 'server');
-  api.add_files('http_access_server.coffee', 'server');
-  api.add_files('resumable_client.coffee', 'client');
-  api.add_files('gridFS_client.coffee', 'client');
+  api.addFiles('gridFS.coffee', ['server','client']);
+  api.addFiles('resumable/resumable.js', 'client')
+  api.addFiles('server_shared.coffee', 'server');
+  api.addFiles('gridFS_server.coffee', 'server');
+  api.addFiles('resumable_server.coffee', 'server');
+  api.addFiles('http_access_server.coffee', 'server');
+  api.addFiles('resumable_client.coffee', 'client');
+  api.addFiles('gridFS_client.coffee', 'client');
   api.export('FileCollection');
   api.export('fileCollection');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use(['fileCollection', 'coffeescript', 'tinytest', 'test-helpers', 'http']);
-  api.add_files('file_collection_tests.coffee', ['server', 'client']);
+  api.addFiles('file_collection_tests.coffee', ['server', 'client']);
 });
