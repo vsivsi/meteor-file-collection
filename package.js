4ef8852ec3ae22ffb9ef967599303af889863b93
Vaughn Iverson <vsivsi@yahoo.com>
2014-09-01 14:49:14 -0700

    Migrated package.js api calls to camelCase. Added version numbers to used internal packages

--- package.js
+++ package.js
@@ -21,22 +21,21 @@ Npm.depends({
   'cookie-parser': '1.3.2'
 });
 
-Package.on_use(function(api) {
-  api.use('coffeescript', ['server','client']);
-  api.use('webapp', 'server');
-  api.add_files('gridFS.coffee', ['server','client']);
-  api.add_files('resumable/resumable.js', 'client')
-  api.add_files('server_shared.coffee', 'server');
-  api.add_files('gridFS_server.coffee', 'server');
-  api.add_files('resumable_server.coffee', 'server');
-  api.add_files('http_access_server.coffee', 'server');
-  api.add_files('resumable_client.coffee', 'client');
-  api.add_files('gridFS_client.coffee', 'client');
+Package.onUse(function(api) {
+  api.use('coffeescript@1.0.0', ['server','client']);
+  api.use('webapp@1.0.0', 'server');
+  api.addFiles('gridFS.coffee', ['server','client']);
+  api.addFiles('resumable/resumable.js', 'client')
+  api.addFiles('server_shared.coffee', 'server');
+  api.addFiles('gridFS_server.coffee', 'server');
+  api.addFiles('resumable_server.coffee', 'server');
+  api.addFiles('http_access_server.coffee', 'server');
+  api.addFiles('resumable_client.coffee', 'client');
+  api.addFiles('gridFS_client.coffee', 'client');
   api.export('FileCollection');
-  api.export('fileCollection');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use(['vsivsi:file-collection', 'coffeescript', 'tinytest', 'test-helpers', 'http']);
-  api.add_files('file_collection_tests.coffee', ['server', 'client']);
+  api.addFiles('file_collection_tests.coffee', ['server', 'client']);
 });
